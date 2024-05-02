344ce96cd621bc9714b1677b3f2bf8c193bf1d2d
Arcadae <161068051+Arcadae@users.noreply.github.com>
2024-05-02 22:10:59 +0400

    Add files via upload

---
+++ Project6.py
@@ -0,0 +1,122 @@
+import timeit
+import itertools
+
+def get_integer_value(txt: str) -> int:
+    while True:
+        try:
+            value = int(input('Number of chess players'))
+            if value==0:
+                raise ValueError
+            return value
+        except ValueError:
+            print(f'This number of people is wrong')
+            
+def digit_to_2(number: int) -> bool:
+    if number%2==0:
+        return True 
+    return False
+    
+def first_var(first_dig: bool , sec_dig: bool , digit_to_2) -> bool:
+    if digit_to_2(first_dig) == True and digit_to_2(sec_dig) == False:
+        return True 
+    return False
+
+def sec_var(first_dig: bool , sec_dig: bool , digit_to_2) -> bool:
+    if digit_to_2(first_dig) == False and digit_to_2(sec_dig) == True:
+        return True 
+    return False
+
+def time_counter(func):
+    def wrapper(*args,**kwargs):
+        start_time = timeit.default_timer()
+        result = func(*args,**kwargs)
+        execution_time = timeit.default_timer() - start_time
+        print(f'\nExecution time for {func.__name__}:',
+        f'{execution_time:.9f} seconds')
+        return result
+    return wrapper
+
+@time_counter
+def method_with_python_function(number: int) -> None:
+    players = range(1, number+1)
+    pairings = list(enumerate(itertools.combinations(players, 2)))
+
+    for round in range(number-1):
+        cache = {}
+        for var in range(1,number):
+            print(f'\nVariation - {var}')
+            played = []
+            for index,pairing in pairings:
+                if pairing[0] not in played and pairing[1] not in played and index not in cache:
+                    print(f'Round number {round+1}: Playing {pairing[0]} and {pairing[1]}')
+                    played.extend(pairing)
+                    cache[index] = index
+                    #print(cache)
+
+
+@time_counter
+def algebraic_method(number: int) -> None:
+    schedules = []
+    for i in range(1, number + 1):
+        for j in range(i + 1, number + 1):
+             schedules.append((i, j))
+    schedules = list(enumerate(schedules,start=1))
+    
+    for round in range(number-1):
+        cache = {}
+        for var in range(1,number):
+            print(f'\nVariation - {var}')
+            played = []
+            for index,pairing in schedules:
+                if pairing[0] not in played and pairing[1] not in played and index not in cache:
+                    print(f'Round number {round+1}: Playing {pairing[0]} and {pairing[1]}')
+                    played.extend(pairing)
+                    cache[index] = index
+
+@time_counter
+def method_with_python_function_sec_approach(number: int) -> None:
+    players = range(1, number+1)
+    pairings = list(enumerate(itertools.combinations(players, 2)))
+
+    for round in range(number-1):
+        cache = {}
+        for var in range(1,number):
+            print(f'\nVariation - {var}')
+            played = []
+            for index,pairing in pairings:
+                if pairing[0] not in played and pairing[1] not in played and index not in cache:
+                    if first_var(pairing[0],pairing[1],digit_to_2)or sec_var(pairing[0],pairing[1],digit_to_2):
+                        print(f'Round number {round+1}: Playing {pairing[0]} and {pairing[1]}')
+                        played.extend(pairing)
+                        cache[index] = index
+                        
+@time_counter
+def algebraic_method_sec_approach(number: int) -> None:
+    schedules = []
+    for i in range(1, number + 1):
+        for j in range(i + 1, number + 1):
+             schedules.append((i, j))
+    schedules = list(enumerate(schedules,start=1))
+    
+    for round in range(number-1):
+        cache = {}
+        for var in range(1,number):
+            print(f'\nVariation - {var}')
+            played = []
+            for index,pairing in schedules:
+                if pairing[0] not in played and pairing[1] not in played and index not in cache:
+                    if first_var(pairing[0],pairing[1],digit_to_2)or sec_var(pairing[0],pairing[1],digit_to_2):
+                        print(f'Round number {round+1}: Playing {pairing[0]} and {pairing[1]}')
+                        played.extend(pairing)
+                        cache[index] = index
+    
+        
+def main():
+    n = get_integer_value('N = ')
+    method_with_python_function(n)
+    method_with_python_function_sec_approach(n)
+    algebraic_method(n)
+    algebraic_method_sec_approach(n)
+    
+if __name__ == '__main__':
+    main()

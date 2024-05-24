b5f2e6478ef4fcab531f1b159fede5a62023a287
Arcadae <161068051+Arcadae@users.noreply.github.com>
2024-05-24 22:15:26 +0400

    Add files via upload

--- Project6.py
+++ Project6.py
@@ -1,131 +1,132 @@
-"""
-1 часть – написать программу в соответствии со своим вариантом задания. Написать 2 варианта формирования (алгоритмический и с помощью функций Питона), сравнив по времени их выполнение.
-2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов (которое будет сокращать количество переборов):(Могут играть только два разных по
-чётности номера) и целевую функцию для нахождения оптимального решения
-Вариант 20. В шахматном турнире принимают участие N шахматистов, причем каждый из них должен сыграть только одну партию с каждым из остальных. Выведите все возможные расписания турнира.
-"""
-
-import itertools 
-import random
+import itertools
 import timeit
-import functools
+import random
 
 def get_integer_value(txt: str) -> int:
     while True:
         try:
-            value = int(input('Number of chess players'))
-            if value==0:
-                raise ValueError
+            value = int(input("Введите количество шахматистов (N): "))
+            assert 4 <= value
             return value
+        except AssertionError:
+            print('Неудачное количество игроков , не получится сыграть турнир.')
         except ValueError:
-            print(f'This number of people is wrong')
+            print('Нецелое количество игроков , почему?')
+            
+def get_no_zero_days(txt: str) -> int:
+    while True:
+        try:
+            days = int(input("Введите количество дней турнира: "))
+            assert 0 < days
+            return days 
+        except AssertionError:
+            print('Как расположить турнир в ноль дней?')
+        except ValueError:
+            print('Вы ввели нецелое количество дней , забавно')
 
-def profile(func):
-    @functools.wraps(func)
-    def inner(*args, **kwargs):
-        start_time = timeit.default_timer()
-        res = func(*args, **kwargs)
-        elapsed_time = timeit.default_timer() - start_time
-        inner.__total_time__ += elapsed_time
-        return res
-    
-    inner.__total_time__ = 0
-    return inner
+def generate_matches_algorithmic(n: int) -> list:
+    matches = []
+    for i in range(n):
+        for j in range(i+1, n):
+            matches.append((i+1, j+1))
+    matches = list(enumerate(matches, start = 1))
+    return matches
 
-def swap_random(seq):
+def generate_matches_itertools(n: int) -> list:
+    matches = list((itertools.combinations(range(1, n+1), 2)))
+    matches = list(enumerate(matches, start = 1))
+    return matches
+
+def swap_random(seq) -> None:
     idx = range(len(seq))
     i1, i2 = random.sample(idx, 2)
     seq[i1], seq[i2] = seq[i2], seq[i1]
     
-def n_permutations(seq: str) -> int:
-    count = 0
-    for i in itertools.product(seq, repeat=2):
-        count += 1 
-    return count
-
-def digit_to_2(number: int) -> bool:
-    if number%2==0:
-        return True 
-    return False
-    
-def first_var(first_dig: int , sec_dig: int , digit_to_2) -> bool:
-    if digit_to_2(first_dig) == True and digit_to_2(sec_dig) == False:
-        return True 
-    return False
-
-def sec_var(first_dig: int , sec_dig: int , digit_to_2) -> bool:
-    if digit_to_2(first_dig) == False and digit_to_2(sec_dig) == True:
-        return True 
-    return False
-
-def is_different_parity(first_dig: int , sec_dig: int) -> bool:
-    if first_var(first_dig, sec_dig, digit_to_2) or sec_var(first_dig, sec_dig, digit_to_2):
-        return True
-    return False
-
-@profile
-def method_with_python(n: int) -> list:
-    players = range(1,n+1)
-    pairs = list(enumerate(itertools.combinations(players, 2), start = 1))
-    return pairs
-
-@profile    
-def alg_method(n: int) -> list:
-    pairs = []
-    for i in range(1, n+ 1):
-        for j in range(i + 1, n + 1):
-             pairs.append((i, j))
-    pairs = list(enumerate(pairs, start = 1))
-    return pairs
-
-def schedule_constructor(pairs: list) -> int:
+def schedule_constructor(pairs: list) -> list:
     for var in range(3):
-        print(f"\t|\Variation No.{var+1}/|\t")
+        played_matches = []
         played_pairs = {}
         round = 0
         while len(played_pairs) != len(pairs):
             round += 1
-            print(f"||Round {round}||")
             played = []
             for index, pair in pairs:
                 if pair[0] not in played and pair[1] not in played and index not in played_pairs:
                     played.extend(pair)
-                    print(f"<<Couple::{', '.join(str(x) for x in played[-2:])}>>", end = "  ")
                     played_pairs[index] = index
-            print()
+                    played_matches.append((pair, round))
+                    sorted_matches = list(sorted(played_matches, key = lambda match: match[1]))
+        #print(', '.join(str(x) for x in sorted_matches))
+        
         swap_random(pairs)
-        
-    return(f"The number of possible permutations in the schedule = {n_permutations(pairs)}")
+    return sorted_matches
     
-def schedule_constructor_with_limitations(pairs: list, n: int) -> int:
-    played_pairs = {}
-    round = 0
-    while round != n-2:
-        round += 1
-        print(f"||Round {round}||")
-        played = []
-        for index, pair in pairs:
-            if pair[0] not in played and pair[1] not in played and index not in played_pairs:
-                if is_different_parity(pair[0], pair[1]):
-                    played.extend(pair)
-                    print(f"<<Couple::{', '.join(str(x) for x in played[-2:])}>>", end = "  ")
-                    played_pairs[index] = index
-        print()
-        
-    return(f"The number of possible permutations in the schedule = {n_permutations(played_pairs)}")   
-        
-def main():
-    n = get_integer_value('N = ')
-    print("Schedules using the itertools method: ")
-    print(f"{schedule_constructor(method_with_python(n))}")
-    print('\n')
-    print("Schedules using the algebraic method: ")
-    print(f"{schedule_constructor(alg_method(n))}")
-    print('\n')
-    print(f"Probably the optimal schedule:")
-    print(f"{schedule_constructor_with_limitations(method_with_python(n), n)}")
-    print('\n')
-    print(f"\nTime to complete {method_with_python.__name__} = {method_with_python.__total_time__:.6f} || {alg_method.__name__} = {alg_method.__total_time__:.6f}")
+def generate_availability(n: int, days: int) -> dict:
+    availability = {}
+    for i in range(1, n + 1):
+        availability[i] = set(random.sample(range(1, days + 1), k=random.randint(1, days)))
+    return availability
+    
+def filter_matches(matches: list, availability: list) -> list:
+    filtered_matches = []
+    for match in matches:
+        if availability[match[0]] and availability[match[1]]: 
+            filtered_matches.append(match)
+    return filtered_matches
 
+def find_optimal_schedule(matches: list, availability: list) -> list:
+    schedule = []
+    used_days = set()
+    for match in matches:
+        common_days = availability[match[0]] & availability[match[1]]
+        if common_days:
+            day = min(common_days)
+            schedule.append((match, day))
+            used_days.add(day)
+        sorted_schedule = list(sorted(schedule, key = lambda schedule: schedule[1]))    
+    return sorted_schedule, len(used_days)    
+
+def main():
+    n = get_integer_value('')
+    days = get_no_zero_days('')
+
+    availability = generate_availability(n, days)
+    matches = list(itertools.combinations(range(1, n + 1), 2))
+    matches = filter_matches(matches, availability)
+
+    start_time = timeit.default_timer()
+    matches_alg = schedule_constructor(generate_matches_algorithmic(n))
+    end_time = timeit.default_timer()
+    alg_time = end_time - start_time
+
+    start_time = timeit.default_timer()
+    matches_itertools = schedule_constructor(generate_matches_itertools(n))
+    end_time = timeit.default_timer()
+    itertools_time = end_time - start_time
+
+    start_time = timeit.default_timer()
+    optimal_schedule, total_days = find_optimal_schedule(matches, availability)
+    end_time = timeit.default_timer()
+    
+    print('Алгоритмич.','||','Итертульный')
+    
+    for a, b in zip(matches_alg, matches_itertools):
+        print(a,'||',b)
+
+    print(f"Алгоритмический подход: {alg_time:.6f} секунд")
+    #print("Матчи:", ', '.join(str(x) for x in matches_alg))
+
+    print(f"Подход с itertools: {itertools_time:.6f} секунд")
+    #print("Матчи:", ', '.join(str(x) for x in matches_itertools))
+
+    print(f"Время на нахождение оптимального расписания: {end_time - start_time:.6f} секунд")
+    print(f"Оптимальное расписание ((партия), день): {','.join(str(x) for x in optimal_schedule)}")
+    print(f"Общее количество дней для проведения турнира: {total_days}")
+
+    if alg_time < itertools_time:
+        print(f"Алгоритмический подход быстрее на {itertools_time - alg_time:.6f} секунд")
+    else:
+        print(f"Подход с itertools быстрее на {alg_time - itertools_time:.6f} секунд")
+       
 if __name__ == '__main__':
     main()
